cb0aba98aba0bf26b0febbe423ae00b472224ee8
Galactic <72170182+TheGalacticAce@users.noreply.github.com>
2021-04-27 15:38:49 -0700

    Added some code that doesn't even work

---
+++ commands/help.js
@@ -0,0 +1,6 @@
+module.exports; {
+    name:'help',
+    execute(message, args); {
+        message.channel.send('The help command is currently under development and unavailable.')
+    }
+}
--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 const Discord = require('discord.js');
 const prefix = '~';
 const client = new Discord.Client();
+const fs = require ('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.command.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log("Bot has started up!");
     console.log("Remember to use 'Crtl + C' to shut down the bot!");
 });
 
-client.on('message', message =>{
+client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
     const args = message.content.slice(prefix.length).split("/ +/");
-    const commands = args.shift().toLowerCase();
+    const command = args.shift().toLowerCase();
 
     if(commands === 'ping'){
-        message.channel.send('Pong!')
+        client.commands.get('ping').execute(message, args);
+    }   
+
+    else if (commands === 'help') {
+        client.commands.get('help').execute(message, args);
     }
 });
 
-client.login("priv");
+
+client.login("<token>");
